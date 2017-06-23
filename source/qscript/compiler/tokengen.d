39454f02d53a937e12514f48bab7644b0ae1c5c1
Nafees <nafeeshassan12345@gmail.com>
2017-06-23 10:49:41 +0500

    `toToken` and `separateTokens` are complete, needs debugging and unittests

--- source/qscript/compiler/tokengen.d
+++ source/qscript/compiler/tokengen.d
@@ -11,14 +11,33 @@ debug{
 	import std.stdio;
 }
 
+package struct TokenList{
+	Token[] tokens; /// Stores the tokens
+	uinteger[] tokenPerLine; /// Stores the number of tokens in each line
+	/// Returns the line number a token is in by usin the index of the token in `tokens`
+	/// 
+	/// The returned value is the line-number, not index, it starts from 1, not zero
+	uinteger getTokenLine(uinteger tokenIndex){
+		uinteger i = 0, chars = 0;
+		tokenIndex ++;
+		for (; chars < tokenIndex && i < tokenPerLine.length; i ++){
+			chars += tokenPerLine[i];
+		}
+		return i;
+	}
+}
+
 /// Reads script, and separates tokens
-private string[] separateTokens(List!string script){
+private TokenList separateTokens(List!string script){
 	LinkedList!string tokens = new LinkedList!string;
-	//First convert everything to 'tokens', TokenType will be set later
+	uinteger[] tokenPerLine;
+	tokenPerLine.length = script.length;
+	// make space in lineTokenCount to store number of tokens in each line, which will be used in error-reporting
 	for (uinteger lineno=0, lineCount = script.length; lineno < lineCount; lineno++){
 		string line = script.read(lineno);
 		bool prevIsIdent = false; /// Stores if the previous char read was present in IDENT_CHARS
 		bool isIdent; /// Stores if the current char is present in IDENT_CHARS
+		uinteger tokenCount = tokens.count;
 		for (uinteger i = 0, readFrom = 0; i < line.length; i ++){
 			// stop at tabs, spaces, and line-endings
 			if (line[i] == ' '|| line[i] == '\t' || i == line.length-1){
@@ -83,16 +102,30 @@ private string[] separateTokens(List!string script){
 			}
 			prevIsIdent = isIdent;
 		}
+		tokenCount = tokens.count - tokenCount;
+		tokenPerLine[lineno] = tokenCount;
+	}
+	// check if error-free
+	if (compileErrors.count == 0){
+		// put all tokens in a Token[] from string[]
+		TokenList list;
+		list.tokens.length = tokens.count;
+		uinteger i = 0;
+		tokens.resetRead;
+		string* ptr = tokens.read;
+		while (ptr !is null && i < list.tokens.length){
+			list.tokens[i].token = *ptr;
+			i ++;
+		}
+		// put in tokenPerLine
+		list.tokenPerLine = tokenPerLine;
+		return list;
+	}else{
+		TokenList list;
+		list.tokens = null;
+		return list;
 	}
 
-	// return tokens if no error
-	string[] r = tokens.toArray;
-	tokens.destroy();
-	if (compileErrors.count == 0){
-		return r;
-	}else{
-		return null;
-	}
 }
 ///
 unittest{
@@ -101,7 +134,7 @@ unittest{
 
 /// Takes script, and separates into tokens (using `separateTokens`), identifies token types, retuns the Tokens with TokenType
 /// in an array
-package Token[] toTokens(List!string script){
+package TokenList toTokens(List!string script){
 	/// Returns true if a string has chars that only identifiers can have
 	bool isIdentifier(string s){
 		return (cast(char[])s).matchElements(cast(char[])IDENT_CHARS);
@@ -117,38 +150,36 @@ package Token[] toTokens(List!string script){
 		}
 		return r;
 	}
-	string[] tokenStrings = separateTokens(script);
-	if (tokenStrings == null){
+	TokenList tokens = separateTokens(script);
+	if (tokens.tokens == null){
 		// there's error
-		return null;
+		return tokens;
 	}else{
 		// continue with identiying tokens
-		Token[] tokens;
-		tokens.length = tokenStrings.length;
 		// fill in tokens with tokenStrings' strings, and identify their type
-		foreach(i, token; tokenStrings){
-			tokens[i].token = token;
+		foreach(i, token; tokens.tokens){
 			// identify type
-			if (isIdentifier(token)){
-				tokens[i].type = TokenType.Identifier;
-			}else if (isOperator(token)){
-				tokens[i].type = TokenType.Operator;
-			}else if (token[0] == '"'){
-				tokens[i].type = TokenType.String;
-			}else if (token.isNum){
-				tokens[i].type = TokenType.Number;
-			}else if (token == ";"){
-				tokens[i].type = TokenType.StatementEnd;
-			}else if (token == ","){
-				tokens[i].type = TokenType.Comma;
-			}else if (["{", "[", "("].hasElement(token)){
-				tokens[i].type = TokenType.BracketOpen;
-			}else if ([")", "]", "}"].hasElement(token)){
-				tokens[i].type = TokenType.BracketClose;
+			if (isIdentifier(token.token)){
+				tokens.tokens[i].type = TokenType.Identifier;
+			}else if (isOperator(token.token)){
+				tokens.tokens[i].type = TokenType.Operator;
+			}else if (token.token[0] == '"'){
+				tokens.tokens[i].type = TokenType.String;
+			}else if (token.token.isNum){
+				tokens.tokens[i].type = TokenType.Number;
+			}else if (token.token == ";"){
+				tokens.tokens[i].type = TokenType.StatementEnd;
+			}else if (token.token == ","){
+				tokens.tokens[i].type = TokenType.Comma;
+			}else if (["{", "[", "("].hasElement(token.token)){
+				tokens.tokens[i].type = TokenType.BracketOpen;
+			}else if ([")", "]", "}"].hasElement(token.token)){
+				tokens.tokens[i].type = TokenType.BracketClose;
 			}else{
-				CompileError error = CompileError(0/*TODO: get token-index-to-lineno-conversion*/, "unidentified token type");
+				CompileError error = CompileError(tokens.getTokenLine(i), "unidentified token type");
 			}
 		}
+		return tokens;
 	}
 }
 
