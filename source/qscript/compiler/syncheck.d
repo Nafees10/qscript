f8cc02bc4941736fd3c4ac59f2daf5f585ef1b7c
Nafees <nafeeshassan12345@gmail.com>
2017-06-28 00:53:57 +0500

    all syntax checking functions except for `checkBlock` and `checkFunctionCallArgs` are complete TODO: add unit tests for these

--- source/qscript/compiler/syncheck.d
+++ source/qscript/compiler/syncheck.d
@@ -28,6 +28,7 @@ private enum TokenSets{
 /// `startIndex` is index in `tokens` from which checking will start, tokens before that index are ignored
 private bool matchTokenTypes(TokenList tokens, TokenType[] types, uinteger startIndex){
 	bool hasError = false;
+	/// TODO: add a length check to `tokens` against `types.length`
 	for (uinteger i = 0; i < types.length; i ++){
 		if (tokens.tokens[startIndex + i].type != types[i]){
 			// not matching
@@ -41,48 +42,92 @@ private bool matchTokenTypes(TokenList tokens, TokenType[] types, uinteger start
 
 /// Checks a function (definition) for syntax errors
 /// Adds error, if any, to misc.compileErrors
-/// Returns the number of tokens occupied by the function
+/// Returns the number of tokens occupied by the block, this number can be wrong in case there was an error
 /// 
-/// tokens is he TokenList containing the whole script's tokens
+/// tokens is the TokenList containing the whole script's tokens
 /// index is the index of the first token for to-be-checked
 private uinteger checkFunctionDefinition(TokenList tokens, uinteger index){
-	TokenType[] expectedTokens = [TokenType.Identifier, TokenType.BlockStart];// afer this comes the function body which will be checked by `checkBlock`
-	bool hasError = false;
-	// make sure that expectedTokens match
-	hasError = matchTokenTypes(tokens, expectedTokens, index);
-	// check if brackets are properly closed, if not, the `bracketPos` will itself put an error in `compileErrors`
-	tokens.bracketPos(index + 1);
-	return expectedTokens.length + checkBlock(tokens, index + 1);
+	TokenType[] expectedTypes = [TokenType.Identifier, TokenType.BlockStart];
+	matchTokenTypes(tokens, expectedTypes, index);
+	// no need to check {} brackets, checkBlock will do that
+	return expectedTypes.length + checkBlock(tokens, index + 1);
 }
 
 /// Checks a Function call for syntax errors
 /// Adds error, if any, to misc.compileErrors
-/// Returns the number of tokens occupied by the function call
+/// Returns the number of tokens occupied by the block, this number can be wrong in case there was an error
 /// 
-/// tokens is he TokenList containing the whole script's tokens
+/// tokens is the TokenList containing the whole script's tokens
 /// index is the index of the first token for to-be-checked
 private uinteger checkFunctionCall(TokenList tokens, uinteger index){
-	
+	TokenType[] expectedTypes = [TokenType.Identifier, TokenType.ParanthesesOpen];
+	matchTokenTypes(tokens, expectedTypes, index);
+	// no need to check for brackets closing, checkArguments does that
+	return expectedTypes.length + checkFunctionCallArgs(tokens, index + 1);
+}
+
+/// Checks a Function call for syntax errors
+/// Adds error, if any, to misc.compileErrors
+/// Returns the number of tokens occupied by the block, this number can be wrong in case there was an error
+/// 
+/// tokens is the TokenList containing the whole script's tokens
+/// index is the index of the first token for to-be-checked
+private uinteger checkFunctionCallArgs(TokenList tokens, uinteger index){
+
 }
 
 /// Checks an if/while statement for syntax errors
 /// Adds error, if any, to misc.compileErrors
-/// Returns the number of tokens occupied by the statement
+/// Returns the number of tokens occupied by the block, this number can be wrong in case there was an error
 /// 
-/// tokens is he TokenList containing the whole script's tokens
+/// tokens is the TokenList containing the whole script's tokens
 /// index is the index of the first token for to-be-checked
 private uinteger checkIfWhileStatement(TokenList tokens, uinteger index){
-	
+	// make sure it's an if/while statement
+	if (tokens.tokens[index].token == "if" || tokens.tokens[index].token == "while"){
+		// continue with the checks
+		TokenType[] expectedTypes = [TokenType.Identifier, TokenType.ParanthesesOpen];
+		if (matchTokenTypes(tokens, expectedTypes, index)){
+			// checks if brackets are properly closed
+			uinteger brackEnd = bracketPos(tokens, index + 1);
+			if (brackEnd >= 0){
+				// make sure it's followed by a	block
+				if (tokens.tokens[brackEnd + 1].type == TokenType.BlockStart){
+					//check if block has closing bracket, return the number of tokens
+					return (tokens.bracketPos(brackEnd + 1) - index) + 1;
+				}
+			}
+		}
+	}else{
+		compileErrors.append(CompileError(tokens.getTokenLine(index), "not an if/while statement"));
+	}
+	return 0;
 }
 
 /// Checks a block-of-code (i.e `{this}`) for syntax errors
 /// Adds error, if any, to misc.compileErrors
-/// Returns the number of tokens occupied by the block
+/// Returns the number of tokens occupied by the block, this number can be wrong in case there was an error
 /// 
-/// tokens is he TokenList containing the whole script's tokens
+/// tokens is the TokenList containing the whole script's tokens
 /// index is the index of the first token for to-be-checked
 private uinteger checkBlock(TokenList tokens, uinteger index){
-	
+	// make sure there are curly brackets
+	if (tokens.tokens[index].type == TokenType.BlockStart){
+		// check if it is properly closed
+		uinteger blockEnd = tokens.bracketPos(index);
+		if (blockEnd >= 0){
+			// check each and every statement
+			for (uinteger i = index + 1; i < tokens.tokens.length; i ++){
+				// here is where the whole syntax is checked
+
+			}
+
+		}//else{} not required coz bracketPos itself appends an error in case on one
+		return (blockEnd - index) + 1; // +1 for the first bracket
+	}else{
+		compileErrors.append(CompileError(tokens.getTokenLine(index), "'{' expected"));
+		return 0;
+	}
 }
 
 package void checkSyntax(TokenList tokens){
