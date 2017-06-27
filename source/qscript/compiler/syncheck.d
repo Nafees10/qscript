970b2daa4007d5218e2301fee2586f5b6e0c2a18
Nafees <nafeeshassan12345@gmail.com>
2017-06-27 13:05:40 +0500

    Finished `checkFunctionDefinition`, needs unit tests

--- source/qscript/compiler/syncheck.d
+++ source/qscript/compiler/syncheck.d
@@ -24,28 +24,52 @@ private enum TokenSets{
 /// Checks a function (definition) for syntax errors
 /// Adds error, if any, to misc.compileErrors
 /// Returns the number of tokens occupied by the function
-private uinteger checkFunctionDefinition(TokenList tokens){
-	
+/// 
+/// tokens is he TokenList containing the whole script's tokens
+/// index is the index of the first token for to-be-checked
+private uinteger checkFunctionDefinition(TokenList tokens, uinteger index){
+	TokenType[] expectedTokens = [TokenType.Identifier, TokenType.BlockStart];// afer this comes the function body which will be checked by `checkBlock`
+	bool hasError = false;
+	// make sure that expectedTokens match
+	for (uinteger i = 0; i < expectedTokens.length; i ++){
+		if (tokens.tokens[index + i].type != expectedTokens[i]){
+			//add error, and break
+			compileErrors.append(CompileError(tokens.getTokenLine(index + i), "syntax error, unexpected token"));
+			hasError = true;
+		}
+	}
+	// check if brackets are properly closed, if not, the `bracketPos` will itself put an error in `compileErrors`
+	tokens.bracketPos(index + 1);
+	return expectedTokens.length + checkBlock(tokens, index + 1);
 }
 
 /// Checks a Function call for syntax errors
 /// Adds error, if any, to misc.compileErrors
 /// Returns the number of tokens occupied by the function call
-private uinteger checkFunctionCall(TokenList tokens){
+/// 
+/// tokens is he TokenList containing the whole script's tokens
+/// index is the index of the first token for to-be-checked
+private uinteger checkFunctionCall(TokenList tokens, uinteger index){
 	
 }
 
 /// Checks an if/while statement for syntax errors
 /// Adds error, if any, to misc.compileErrors
 /// Returns the number of tokens occupied by the statement
-private uinteger checkIfWhileStatement(TokenList tokens){
+/// 
+/// tokens is he TokenList containing the whole script's tokens
+/// index is the index of the first token for to-be-checked
+private uinteger checkIfWhileStatement(TokenList tokens, uinteger index){
 	
 }
 
 /// Checks a block-of-code (i.e `{this}`) for syntax errors
 /// Adds error, if any, to misc.compileErrors
 /// Returns the number of tokens occupied by the block
-private uinteger checkBlock(TokenList tokens){
+/// 
+/// tokens is he TokenList containing the whole script's tokens
+/// index is the index of the first token for to-be-checked
+private uinteger checkBlock(TokenList tokens, uinteger index){
 	
 }
 
