ecb4896acfe585d332ef5f77167bde7faa042e35
Nafees <nafeeshassan12345@gmail.com>
2017-08-04 18:30:14 +0500

    Removed checks for wrong-brackets, because those errors will be caught in tokengen

--- source/qscript/compiler/ast.d
+++ source/qscript/compiler/ast.d
@@ -140,13 +140,7 @@ struct ASTGen{
 			// make sure it's a block
 			if (tokens.tokens[index].type == Token.Type.BlockStart){
 				uinteger brackEnd = tokens.tokens.bracketPos!(true)(index);
-				if (brackEnd >= 0){
-					// everything's good
-					// use generateStatementsAST to generateAST for statements
-					blockNode.addSubNode(generateStatementsAST(tokens, index, brackEnd-1));
-				}else{
-					// an error that should have been killed in tokengen escaped here :(
-				}
+				blockNode.addSubNode(generateStatementsAST(tokens, index+1, brackEnd-1));
 			}else{
 				compileErrors.append(CompileError(tokens.getTokenLine(index), "not a block"));
 			}
@@ -175,27 +169,7 @@ struct ASTGen{
 					}
 				}else{
 					// go through and check if it's an assignment
-					for (uinteger i = index; i <= endIndex; i ++){
-						if (tokens.tokens[i].type != Token.Type.Identifier){
-							if (tokens.tokens[i].type == Token.Type.IndexBracketOpen){
-								// is a valid statement till now
-								i = tokens.tokens.bracketPos(i);
-								if (i >= 0){
-									// brackets are ok, check if is an assignment statement
-									if (i < endIndex && tokens.tokens[i+1].type == Token.Type.Operator && 
-										tokens.tokens[i+1].token == "="){
-										return StatementType.Assignment;
-									}else{
-										compileErrors.append(CompileError(tokens.getTokenLine(i), "not an assignment statement"));
-									}
-								}
-							}else{
-								compileErrors.append(CompileError(tokens.getTokenLine(i), "not an assignment statement"));
-							}
-						}else{
-							compileErrors.append(CompileError(tokens.getTokenLine(i), "not an assignment statement"));
-						}
-					}
+					// TODO implement a check to see if it's an asignment statement
 				}
 				return StatementType.NoValidType;
 			}
@@ -217,9 +191,6 @@ struct ASTGen{
 					nodeList.append(generateBlockAST(tokens, i));
 					// skip the block's body
 					i = tokens.tokens.bracketPos(i);
-					if (i == -1){
-						break;
-					}
 				}
 			}
 			statementNodes = nodeList.toArray;
@@ -232,8 +203,11 @@ struct ASTGen{
 			if (tokens.tokens[index].type == Token.Type.Identifier && tokens.tokens[index + 1].type == Token.Type.ParanthesesOpen){
 				// check if bracket is closed and there's a semicolon
 				uinteger brackEnd = tokens.tokens.bracketPos(index + 1);
-				if (brackEnd >= 0 && brackEnd+1 <= endIndex && tokens.tokens[brackEnd+1].type == Token.Type.StatementEnd){
-					ASTNode functionCallNode = ASTNode(ASTNode.Type.FunctionCall, tokens.tokens[index].token, tokens.getTokenLine(index));
+				if (brackEnd+1 <= endIndex && tokens.tokens[brackEnd+1].type == Token.Type.StatementEnd){
+					ASTNode functionCallNode = ASTNode(ASTNode.Type.FunctionCall,
+						tokens.tokens[index].token, 
+						tokens.getTokenLine(index));
+
 					functionCallNode.addSubNode(generateCodeAST(tokens, index+1, brackEnd-1));
 					
 					return functionCallNode;
@@ -258,9 +232,7 @@ struct ASTGen{
 					}else // check if it's a function-call
 					if (i < endIndex && tokens.tokens[i+1].type == Token.Type.ParanthesesOpen){
 						integer brackEnd = tokens.tokens.bracketPos(i+1);
-						if (brackEnd >= 0){
-							lastNode = generateFunctionCallAST(tokens, i, brackEnd);
-						}
+						lastNode = generateFunctionCallAST(tokens, i, brackEnd);
 					}else{
 						// just a var
 						lastNode = ASTNode(ASTNode.Type.Variable, token.token, tokens.getTokenLine(i));
