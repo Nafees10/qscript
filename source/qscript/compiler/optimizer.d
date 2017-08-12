e9b12d3b68e8a843664d9755bfe37121b3771671
Nafees <nafeeshassan12345@gmail.com>
2017-08-12 17:25:14 +0500

    Added a module to do simple checks on ASTNodes, and optimize them a bit
    Signed-off-by: Nafees <nafeeshassan12345@gmail.com>

---
+++ source/qscript/compiler/optimizer.d
@@ -0,0 +1,45 @@
+﻿module qscript.compiler.optimizer;
+
+import qscript.compiler.ast;
+import qscript.compiler.misc;
+import utils.misc;
+
+/// struct providing functions to optimize an check the AST for errors
+struct ASTOptimize{
+	/// optimizes and looks for errors in the scriptNode. returns the optimized scriptNode
+	/// 
+	/// Errors are appended to `qscript.compiler.misc.compileErrors`
+	public ASTNode optimizeScript(ASTNode script){
+		// make sure scriptNode was received
+		if (script.type == ASTNode.Type.Script){
+			// ok
+			// make sure all suNodes are functions, and call functions to optimize & check those nodes as well
+			ASTNode[] subNodes = script.getSubNodes();
+			foreach (subNode; subNodes){
+
+			}
+		}else{
+			compileErrors.append(CompileError("ASTOptimize.optimizeScript can only be called with a node of type script"));
+			return script;
+		}
+	}
+
+	/// optimizes and looks for errors in functions nodes
+	private ASTNode optimizeFunction(ASTNode functionNode){
+		// make sure it is a function
+		if (functionNode.type == ASTNode.Type.Function){
+			// ok
+			// check and optimize each and every node
+
+		}else{
+			compileErrors.append(CompileError("ASTOptimize.optimizeFunction can only be called with a node of type function"));
+			return functionNode;
+		}
+	}
+
+	/// optimizes and looks for errors in a statement
+	private ASTNode optimizeStatement(ASTNode statement){
+		// make sure it is a statement
+		// TODO continue from here
+	}
+}
