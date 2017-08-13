e67593e4d3558a65e1529a5427eb52c5366433e5
Nafees <nafeeshassan12345@gmail.com>
2017-08-13 09:20:34 +0500

    Added a struct to store variable at compile time

--- source/qscript/compiler/optimizer.d
+++ source/qscript/compiler/optimizer.d
@@ -3,9 +3,31 @@
 import qscript.compiler.ast;
 import qscript.compiler.misc;
 import utils.misc;
+import utils.lists;
+
+/// Used by ASTOptimize to temporarily store information about a var
+struct Var{
+	enum Type{
+		Number,
+		String,
+		Array,
+		Undefined
+	}
+	string name; /// the variable name
+	Type type; /// the type of data stored in the var, if not static, or not known, it is `Var.Type.Undefined`
+	bool valueKnown; /// stores whether the value of the var is static (true) or not (false)
+	union{
+		string stringValue; /// to store value if type==string
+		double numberValue; /// to store value if type==number
+		string[] stringArrayValue; /// to store value if type==array of string
+		double[] numberArrayValue; /// to store value if type==array of number
+	}
+}
 
 /// struct providing functions to optimize an check the AST for errors
-struct ASTOptimize{
+public struct ASTOptimize{
+	/// stores the variable with their types, and value (if static), the index is the name of the var
+	Var[string] vars;
 	/// optimizes and looks for errors in the scriptNode. returns the optimized scriptNode
 	/// 
 	/// Errors are appended to `qscript.compiler.misc.compileErrors`
@@ -30,7 +52,10 @@ struct ASTOptimize{
 		if (functionNode.type == ASTNode.Type.Function){
 			// ok
 			// check and optimize each and every node
+			ASTNode[] statements = functionNode.getSubNodes;
+			foreach (statement; statements){
 
+			}
 		}else{
 			compileErrors.append(CompileError("ASTOptimize.optimizeFunction can only be called with a node of type function"));
 			return functionNode;
