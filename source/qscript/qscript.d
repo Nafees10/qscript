29f0a968bbb4baf5ea22a8f01b7ff41e9b302429
Nafees <nafeeshassan12345@gmail.com>
2017-08-15 12:33:50 +0500

    Added `QData` struct to store data for script, as if in a static typed lang
    Signed-off-by: Nafees <nafeeshassan12345@gmail.com>

--- source/qscript/qscript.d
+++ source/qscript/qscript.d
@@ -1,11 +1,71 @@
 module qscript.qscript;
-/*
+
 import utils.misc;
-import utils.lists;
+/*import utils.lists;
 //import qscript.compiler;
 import std.stdio;
 import std.conv:to;
-
+*/
+public struct QData{
+	enum Type{
+		String,
+		UnsignedInt,
+		SignedInt,
+		Double,
+		Array
+	}
+	private Type dataType;
+	union{
+		string strVal;
+		uinteger uintVal;
+		integer intVal;
+		double doubleVal;
+		QData[] arrayVal;
+	}
+	/// changes the data contained by this struct
+	@property auto value(T)(T val){
+		static if (typeid(T) == typeid(string)){
+			dataType = Type.String;
+			strVal = val;
+		}else static if (typeid(T) == typeid(uinteger)){
+			dataType = Type.UnsignedInt;
+			uintVal = val;
+		}else static if (typeid(T) == typeid(integer)){
+			dataType = Type.SignedInteger;
+			intVal = val;
+		}else static if (typeid(T) == typeid(double) || typeid(T) == typeid(float)){
+			dataType = Type.Double;
+			doubleVal = val;
+		}else static if (typeid(T) == typeid(QData[])){
+			dataType = Type.Array;
+			arrayVal = val;
+		}else{
+			throw new Exception("attempting to store invalid data type in QData");
+		}
+	}
+	/// retrieves the value stored by this struct
+	@property auto value(T)(){
+		/// make sure it is the correct type
+		static if (typeid(T) == typeid(string)){
+			return strVal;
+		}else static if (typeid(T) == typeid(uinteger)){
+			return uintVal;
+		}else static if (typeid(T) == typeid(integer)){
+			return intVal;
+		}else static if (typeid(T) == typeid(double) || typeid(T) == typeid(float)){
+			return doubleVal;
+		}else static if (typeid(T) == typeid(QData[])){
+			return arrayVal;
+		}else{
+			throw new Exception("attempting to retrieve invalid data type from QData");
+		}
+	}
+	/// retrieves the type of data stored in this struct
+	@property Type type(){
+		return dataType;
+	}
+}
+/*
 /// This is the type of each script function, used in this module
 private alias ScriptFunction = QVar delegate(QVar[]);
 /// The type for `onExec`
