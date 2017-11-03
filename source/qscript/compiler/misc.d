1d768256587db78dbec001e4b7aeb5e3669775a5
Nafees <nafeeshassan16@gmail.com>
2017-11-03 18:08:26 +0500

    Fixed a bug with tokengen not reading floats right
    Signed-off-by: Nafees <nafeeshassan16@gmail.com>

--- source/qscript/compiler/misc.d
+++ source/qscript/compiler/misc.d
@@ -9,7 +9,7 @@ import std.range;
 import std.conv : to;
 
 /// An array containing all chars that an identifier can contain
-package const char[] IDENT_CHARS = iota('a', 'z'+1).array~iota('A', 'Z'+1).array~iota('0', '9'+1).array~[cast(int)'_'];
+package const char[] IDENT_CHARS = iota('a', 'z'+1).array~iota('A', 'Z'+1).array~iota('0', '9'+1).array~[cast(int)'_', '.'];
 /// An array containing all keywords
 package const string[] KEYWORDS = ["function", "if", "else", "while", "void", "int", "string", "double"];
 /// data types
