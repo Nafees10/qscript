36256820f06e03ce1521e0414f092e0c851c9d1a
Nafees10 <nafeeshassan16@gmail.com>
2018-11-21 15:47:23 +0500

    Fixed some issues with "JSONValue is not an object"
    Signed-off-by: Nafees10 <nafeeshassan16@gmail.com>

--- source/qscript/compiler/astreadable.d
+++ source/qscript/compiler/astreadable.d
@@ -21,8 +21,8 @@ JSONValue toJSON(ScriptNode node){
 		functionsJSON[i] = fNode.toJSON;
 	}
 	JSONValue r;
-	r.object["node"] = JSONValue("ScriptNode");
-	r.object["functions"] = JSONValue(functionsJSON);
+	r["node"] = JSONValue("ScriptNode");
+	r["functions"] = JSONValue(functionsJSON);
 	return r;
 }
 
@@ -31,16 +31,16 @@ JSONValue toJSON(FunctionNode node){
 	JSONValue[] argListJSON;
 	argListJSON.length = node.arguments.length;
 	foreach (i, arg; node.arguments){
-		argListJSON[i].object["name"] = arg.argName;
-		argListJSON[i].object["type"] = arg.argType.toString;
+		argListJSON[i]["name"] = JSONValue(arg.argName);
+		argListJSON[i]["type"] = JSONValue(arg.argType.toString);
 	}
 	JSONValue bodyJSON = node.bodyBlock.toJSON;
 	JSONValue r;
-	r.object["node"] = JSONValue("FunctionNode");
-	r.object["type"] = node.returnType.toString;
-	r.object["name"] = node.name;
-	r.object["arguments"] = JSONValue(argListJSON);
-	r.object["body"] = JSONValue(bodyJSON);
+	r["node"] = JSONValue("FunctionNode");
+	r["type"] = node.returnType.toString;
+	r["name"] = node.name;
+	r["arguments"] = JSONValue(argListJSON);
+	r["body"] = JSONValue(bodyJSON);
 	return r;
 }
 
@@ -52,31 +52,31 @@ JSONValue toJSON(BlockNode node){
 		statementListJSON[i] = statement.toJSON;
 	}
 	JSONValue r;
-	r.object["node"] = JSONValue("BlockNode");
-	r.object["statements"] = JSONValue(statementListJSON);
+	r["node"] = JSONValue("BlockNode");
+	r["statements"] = JSONValue(statementListJSON);
 	return r;
 }
 
 /// Creates JSONValue for StatementNode
 JSONValue toJSON(StatementNode node){
 	JSONValue r;
-	r.object["node"] = JSONValue("StatementNode");
+	r["node"] = JSONValue("StatementNode");
 	if (node.type == StatementNode.Type.Assignment){
-		r.object["statement"] = node.node!(StatementNode.Type.Assignment).toJSON;
+		r["statement"] = node.node!(StatementNode.Type.Assignment).toJSON;
 	}else if (node.type == StatementNode.Type.Block){
-		r.object["statement"] = node.node!(StatementNode.Type.Block).toJSON;
+		r["statement"] = node.node!(StatementNode.Type.Block).toJSON;
 	}else if (node.type == StatementNode.Type.DoWhile){
-		r.object["statement"] = node.node!(StatementNode.Type.DoWhile).toJSON;
+		r["statement"] = node.node!(StatementNode.Type.DoWhile).toJSON;
 	}else if (node.type == StatementNode.Type.For){
-		r.object["statement"] = node.node!(StatementNode.Type.For).toJSON;
+		r["statement"] = node.node!(StatementNode.Type.For).toJSON;
 	}else if (node.type == StatementNode.Type.FunctionCall){
-		r.object["statement"] = node.node!(StatementNode.Type.FunctionCall).toJSON;
+		r["statement"] = node.node!(StatementNode.Type.FunctionCall).toJSON;
 	}else if (node.type == StatementNode.Type.If){
-		r.object["statement"] = node.node!(StatementNode.Type.If).toJSON;
+		r["statement"] = node.node!(StatementNode.Type.If).toJSON;
 	}else if (node.type == StatementNode.Type.VarDeclare){
-		r.object["statement"] = node.node!(StatementNode.Type.VarDeclare).toJSON;
+		r["statement"] = node.node!(StatementNode.Type.VarDeclare).toJSON;
 	}else if (node.type == StatementNode.Type.While){
-		r.object["statement"] = node.node!(StatementNode.Type.While).toJSON;
+		r["statement"] = node.node!(StatementNode.Type.While).toJSON;
 	}
 	return r;
 }
@@ -84,113 +84,113 @@ JSONValue toJSON(StatementNode node){
 /// Creates JSONValue for AssignmentNode
 JSONValue toJSON(AssignmentNode node){
 	JSONValue r;
-	r.object["node"] = JSONValue("AssignmentNode");
-	r.object["lvalueIsRef"] = JSONValue(node.deref ? "true" : "false");
-	r.object["var"] = node.var.toJSON;
+	r["node"] = JSONValue("AssignmentNode");
+	r["lvalueIsRef"] = JSONValue(node.deref ? "true" : "false");
+	r["var"] = node.var.toJSON;
 	JSONValue[] indexes;
 	indexes.length = node.indexes.length;
 	foreach (i, index; node.indexes){
 		indexes[i] = index.toJSON;
 	}
-	r.object["indexes"] = JSONValue(indexes);
-	r.object["val"] = node.val.toJSON;
+	r["indexes"] = JSONValue(indexes);
+	r["val"] = node.val.toJSON;
 	return r;
 }
 
 /// Creates JSONValue for IfNode
 JSONValue toJSON(IfNode node){
 	JSONValue r;
-	r.object["node"] = JSONValue("IfNode");
-	r.object["condition"] = node.condition.toJSON;
-	r.object["onTrue"] = node.statement.toJSON;
+	r["node"] = JSONValue("IfNode");
+	r["condition"] = node.condition.toJSON;
+	r["onTrue"] = node.statement.toJSON;
 	if (node.hasElse)
-		r.object["onFalse"] = node.elseStatement.toJSON;
+		r["onFalse"] = node.elseStatement.toJSON;
 	return r;
 }
 
 /// Creates JSONValue for WhileNode
 JSONValue toJSON(WhileNode node){
 	JSONValue r;
-	r.object["node"] = JSONValue("WhileNode");
-	r.object["condtion"] = node.condition.toJSON;
-	r.object["statement"] = node.statement.toJSON;
+	r["node"] = JSONValue("WhileNode");
+	r["condtion"] = node.condition.toJSON;
+	r["statement"] = node.statement.toJSON;
 	return r;
 }
 
 /// Creates JSONValue for DoWhileNode
 JSONValue toJSON(DoWhileNode node){
 	JSONValue r;
-	r.object["node"] = JSONValue("DoWhileNode");
-	r.object["condition"] = node.condition.toJSON;
-	r.object["statement"] = node.statement.toJSON;
+	r["node"] = JSONValue("DoWhileNode");
+	r["condition"] = node.condition.toJSON;
+	r["statement"] = node.statement.toJSON;
 	return r;
 }
 
 /// Creates JSONValue for ForNode
 JSONValue toJSON(ForNode node){
 	JSONValue r;
-	r.object["node"] = JSONValue("ForNode");
-	r.object["init"] = node.initStatement.toJSON;
-	r.object["condition"] = node.condition.toJSON;
-	r.object["inc"] = node.incStatement.toJSON;
-	r.object["statement"] = node.statement.toJSON;
+	r["node"] = JSONValue("ForNode");
+	r["init"] = node.initStatement.toJSON;
+	r["condition"] = node.condition.toJSON;
+	r["inc"] = node.incStatement.toJSON;
+	r["statement"] = node.statement.toJSON;
 	return r;
 }
 
 /// Creates JSONValue for FunctionCallNode
 JSONValue toJSON(FunctionCallNode node){
 	JSONValue r;
-	r.object["node"] = JSONValue("FunctionCallNode");
-	r.object["name"] = JSONValue(node.fName);
-	r.object["type"] = JSONValue(node.returnType.toString);
+	r["node"] = JSONValue("FunctionCallNode");
+	r["name"] = JSONValue(node.fName);
+	r["type"] = JSONValue(node.returnType.toString);
 	JSONValue[] argListJSON;
 	argListJSON.length = node.arguments.length;
 	foreach (i, arg; node.arguments){
 		argListJSON[i] = arg.toJSON;
 	}
-	r.object["args"] = JSONValue(argListJSON);
+	r["args"] = JSONValue(argListJSON);
 	return r;
 }
 
 /// Creates JSONValue for VarDeclareNode
 JSONValue toJSON(VarDeclareNode node){
 	JSONValue r;
-	r.object["node"] = JSONValue("varDeclareNode");
-	r.object["type"] = JSONValue(node.type.toString);
+	r["node"] = JSONValue("varDeclareNode");
+	r["type"] = JSONValue(node.type.toString);
 	JSONValue[] varTypeValueList;
 	varTypeValueList.length = node.vars.length;
 	foreach (i, varName; node.vars){
 		JSONValue var;
-		var.object["name"] = JSONValue(varName);
-		var.object["id"] = JSONValue(node.varIDs[varName]);
+		var["name"] = JSONValue(varName);
+		var["id"] = JSONValue(node.varIDs[varName]);
 		if (node.hasValue(varName))
-			var.object["value"] = node.getValue(varName).toJSON;
+			var["value"] = node.getValue(varName).toJSON;
 		varTypeValueList[i] = var;
 	}
-	r.object["vars"] = JSONValue(varTypeValueList);
+	r["vars"] = JSONValue(varTypeValueList);
 	return r;
 }
 
 /// Creates JSONValue for CodeNode
 JSONValue toJSON(CodeNode node){
 	JSONValue r;
-	r.object["node"] = JSONValue("CodeNode");
-	r.object["type"] = JSONValue(node.returnType.toString);
-	r.object["isLiteral"] = JSONValue(node.isLiteral ? "true" : "false");
+	r["node"] = JSONValue("CodeNode");
+	r["type"] = JSONValue(node.returnType.toString);
+	r["isLiteral"] = JSONValue(node.isLiteral ? "true" : "false");
 	if (node.type == CodeNode.Type.Array){
-		r.object["code"] = node.node!(CodeNode.Type.Array).toJSON;
+		r["code"] = node.node!(CodeNode.Type.Array).toJSON;
 	}else if (node.type == CodeNode.Type.FunctionCall){
-		r.object["code"] = node.node!(CodeNode.Type.FunctionCall).toJSON;
+		r["code"] = node.node!(CodeNode.Type.FunctionCall).toJSON;
 	}else if (node.type == CodeNode.Type.Literal){
-		r.object["code"] = node.node!(CodeNode.Type.Literal).toJSON;
+		r["code"] = node.node!(CodeNode.Type.Literal).toJSON;
 	}else if (node.type == CodeNode.Type.Operator){
-		r.object["code"] = node.node!(CodeNode.Type.Operator).toJSON;
+		r["code"] = node.node!(CodeNode.Type.Operator).toJSON;
 	}else if (node.type == CodeNode.Type.ReadElement){
-		r.object["code"] = node.node!(CodeNode.Type.ReadElement).toJSON;
+		r["code"] = node.node!(CodeNode.Type.ReadElement).toJSON;
 	}else if (node.type == CodeNode.Type.SOperator){
-		r.object["code"] = node.node!(CodeNode.Type.SOperator).toJSON;
+		r["code"] = node.node!(CodeNode.Type.SOperator).toJSON;
 	}else if (node.type == CodeNode.Type.Variable){
-		r.object["code"] = node.node!(CodeNode.Type.Variable).toJSON;
+		r["code"] = node.node!(CodeNode.Type.Variable).toJSON;
 	}
 	return r;
 }
@@ -198,66 +198,66 @@ JSONValue toJSON(CodeNode node){
 /// Creates JSONValue for VariableNode
 JSONValue toJSON(VariableNode node){
 	JSONValue r;
-	r.object["node"] = JSONValue("VariableNode");
-	r.object["type"] = JSONValue(node.returnType.toString);
-	r.object["name"] = JSONValue(node.varName);
+	r["node"] = JSONValue("VariableNode");
+	r["type"] = JSONValue(node.returnType.toString);
+	r["name"] = JSONValue(node.varName);
 	return r;
 }
 
 /// Creates JSONValue for ArrayNode
 JSONValue toJSON(ArrayNode node){
 	JSONValue r;
-	r.object["node"] = JSONValue("ArrayNode");
-	r.object["type"] = JSONValue(node.returnType.toString);
-	r.object["isLiteral"] = JSONValue(node.isLiteral ? "true" : "false");
+	r["node"] = JSONValue("ArrayNode");
+	r["type"] = JSONValue(node.returnType.toString);
+	r["isLiteral"] = JSONValue(node.isLiteral ? "true" : "false");
 	JSONValue[] elementList;
 	elementList.length = node.elements.length;
 	foreach (i, element; node.elements){
 		elementList[i] = element.toJSON;
 	}
-	r.object["elements"] = JSONValue(elementList);
+	r["elements"] = JSONValue(elementList);
 	return r;
 }
 
 /// Creates JSONValue for LiteralNode
 JSONValue toJSON(LiteralNode node){
 	JSONValue r;
-	r.object["node"] = JSONValue("LiteralNode");
-	r.object["type"] = JSONValue(node.returnType.toString);
-	r.object["value"] = JSONValue(node.toByteCode);
+	r["node"] = JSONValue("LiteralNode");
+	r["type"] = JSONValue(node.returnType.toString);
+	r["value"] = JSONValue(node.toByteCode);
 	return r;
 }
 
 /// Creates JSONValue for OperatorNode
 JSONValue toJSON(OperatorNode node){
 	JSONValue r;
-	r.object["node"] = JSONValue("OperatorNode");
-	r.object["type"] = JSONValue(node.returnType.toString);
-	r.object["isLiteral"] = JSONValue(node.isLiteral ? "true" : "false");
-	r.object["operator"] = JSONValue(node.operator);
-	r.object["operandA"] = node.operands[0].toJSON;
-	r.object["operandB"] = node.operands[1].toJSON;
+	r["node"] = JSONValue("OperatorNode");
+	r["type"] = JSONValue(node.returnType.toString);
+	r["isLiteral"] = JSONValue(node.isLiteral ? "true" : "false");
+	r["operator"] = JSONValue(node.operator);
+	r["operandA"] = node.operands[0].toJSON;
+	r["operandB"] = node.operands[1].toJSON;
 	return r;
 }
 
 /// Creates JSONValue for SOperatorNode
 JSONValue toJSON(SOperatorNode node){
 	JSONValue r;
-	r.object["node"] = JSONValue("SOperatorNode");
-	r.object["type"] = JSONValue(node.returnType.toString);
-	r.object["isLiteral"] = JSONValue(node.isLiteral ? "true" : "false");
-	r.object["operator"] = JSONValue(node.operator);
-	r.object["operand"] = node.operand.toJSON;
+	r["node"] = JSONValue("SOperatorNode");
+	r["type"] = JSONValue(node.returnType.toString);
+	r["isLiteral"] = JSONValue(node.isLiteral ? "true" : "false");
+	r["operator"] = JSONValue(node.operator);
+	r["operand"] = node.operand.toJSON;
 	return r;
 }
 
 /// Creates JSONValue for ReadElement
 JSONValue toJSON(ReadElement node){
 	JSONValue r;
-	r.object["node"] = JSONValue("ReadElement");
-	r.object["type"] = JSONValue(node.returnType.toString);
-	r.object["isLiteral"] = JSONValue(node.isLiteral ? "true" : "false");
-	r.object["array"] = node.readFromNode.toJSON;
-	r.object["index"] = node.index.toJSON;
+	r["node"] = JSONValue("ReadElement");
+	r["type"] = JSONValue(node.returnType.toString);
+	r["isLiteral"] = JSONValue(node.isLiteral ? "true" : "false");
+	r["array"] = node.readFromNode.toJSON;
+	r["index"] = node.index.toJSON;
 	return r;
 }
