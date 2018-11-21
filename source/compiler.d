07af25050a29dfb1827626bf8b137f9a25ca0171
Nafees10 <nafeeshassan16@gmail.com>
2018-11-21 15:48:24 +0500

    Added module and new config to test out the compiler easily
    Signed-off-by: Nafees10 <nafeeshassan16@gmail.com>

---
+++ source/compiler.d
@@ -0,0 +1,50 @@
+﻿/++
+To create a executable which serves as a front end to just the compiler.  
+
+This can be used to:
+* convert script to AST, and output the AST in JSON
+* convert script to byte code, and output the byte code (to be added)
+* optimize existing byte code, and output new optimized byte code (to be added)
+* convert script to byte code, optimize it, and output optimized byte code (to be added)
++/
+module compiler;
+
+import std.stdio;
+import std.file;
+
+import qscript.compiler.asttest;
+import qscript.compiler.compiler : CompileError;
+
+import utils.misc;
+
+version(compiler){
+	void main(string[] args){
+		if (args.length < 3){
+			writeln ("not enough args. Usage:\n./compiler CompilationType path/to/script");
+			writeln ("CompilationType can be:\n* ast - output AST in JSON");
+		}else if (!["ast"].hasElement(args[1])){
+			writeln ("invalid CompilationType");
+		}else if (!exists(args[2])){
+			writeln ("file "~args[2]~" doesn't exist");
+		}else{
+			string[] script;
+			try{
+				script = fileToArray(args[2]);
+			}catch (Exception e){
+				.destroy(e);
+				writeln ("Error reading file");
+			}
+			CompileError[] errors;
+			if (args[1] == "ast"){
+				string json = astJSONTest(script,[],errors);
+				if (errors.length > 0){
+					stderr.writeln ("There are errors:");
+					foreach (error; errors){
+						stderr.writeln("Line#",error.lineno,": ",error.msg);
+					}
+				}
+				writeln (json);
+			}
+		}
+	}
+}
