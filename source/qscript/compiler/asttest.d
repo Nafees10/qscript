7aa51018a1f86dd9803b823ec2295aec3c98805d
Nafees10 <nafeeshassan16@gmail.com>
2018-11-21 15:48:03 +0500

    Added module to test AST->JSON
    Signed-off-by: Nafees10 <nafeeshassan16@gmail.com>

---
+++ source/qscript/compiler/asttest.d
@@ -0,0 +1,28 @@
+﻿module qscript.compiler.asttest;
+
+import qscript.compiler.compiler;
+import qscript.compiler.ast;
+import qscript.compiler.astgen;
+import qscript.compiler.astcheck;
+import qscript.compiler.astreadable;
+import qscript.compiler.tokengen;
+import qscript.compiler.misc;
+
+import utils.misc;
+import std.json;
+
+public string astJSONTest(string[] script, Function[] preDefFunctions,ref CompileError[] errors){
+	TokenList tokens = toTokens(script, errors);
+	if (errors.length == 0){
+		ASTGen astMake;
+		ScriptNode scrNode = astMake.generateScriptAST(tokens, errors);
+		if (errors.length == 0){
+			ASTCheck check = new ASTCheck(preDefFunctions);
+			errors = check.checkAST(scrNode);
+			.destroy(check);
+			JSONValue jsonAST = scrNode.toJSON;
+			return jsonAST.toPrettyString();
+		}
+	}
+	return "";
+}
