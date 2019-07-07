592e6368f57512cd0b3a911b489ed115c8c7d24e
Nafees10 <nafeeshassan16@gmail.com>
2019-07-07 18:43:16 +0500

    Finalized the structure for ByteCode. Only thing left about it is the functions;
    which is everything
    
    Signed-off-by: Nafees10 <nafeeshassan16@gmail.com>

--- source/qscript/compiler/bytecode.d
+++ source/qscript/compiler/bytecode.d
@@ -12,12 +12,28 @@ import utils.lists;
 import std.conv : to;
 
 /// provides functions for dealing with byte code generate using qscript.compiler.codegen
-public struct ByteCode{
+public class ByteCode{
+	/// stores an element on the fucntion stack. The stack is made by simply making aray of this
+	public struct StackElement{
+		/// possible types of element
+		enum Type{
+			Reference, /// a reference to another element in same stack
+			Literal, /// a literal QData
+		}
+		/// stores the type this StackElement is
+		public Type type;
+		/// stores the literal QData in byte code encoded format. Only valid if type==Type.literal
+		public string literal;
+		/// stores the index of the element being referenced. Only valid if type==Type.Reference
+		public uinteger refIndex;
+	}
 	/// used to store byte code's instruction in a more readable format
 	public struct Instruction{
 		/// instruction name
 		string name;
 		/// arguments of instruction. These are written in the byte code encoded way
+		///
+		/// these are stored as string instead of QData so its possible to generate readable byte code\
 		string[] args;
 	}
 	/// used to store byte code's function in a more readable format
@@ -26,9 +42,17 @@ public struct ByteCode{
 		uinteger id;
 		/// instructions that make up this function
 		Instruction[] instructions;
+		/// The stack that's loaded before this function is executed
+		StackElement[] stack;
 	}
 	/// stores the funtion map. Maps function id to their names
-	string[uinteger] functionMap;
+	public string[uinteger] functionMap;
+	/// stores the functions with their instructions. id is the function id
+	public Function[uinteger] functions;
+	// functions to help codegen generate byte code
+	package{
+		// TODO add functions to help codegen
+	}
 }
 
 // some misc. functions
