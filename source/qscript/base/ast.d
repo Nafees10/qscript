df61357cbe51939c93411afb3ca46dc7e67c7007
Nafees Hassan <nafeeshassan16@gmail.com>
2023-05-30 04:42:24 +0500

    Implemented reading matching units from UDAs

--- source/qscript/base/ast.d
+++ source/qscript/base/ast.d
@@ -6,16 +6,17 @@ import std.traits;
 import std.json;
 import std.string;
 import std.array;
+import std.uni : isWhite;
 import std.stdio;
 import std.conv : to;
 
 /// an AST Node
-// initialise with T = Token Type enum
-public class ASTNode(T) if (is(T == enum)){
+// initialise with T = Token Type enum, M = Match Type enum
+public class ASTNode(T, M) if (is(T == enum) && is(M == enum)){
 public:
 	Token!T token;
 	ASTNode[] childNodes;
-	T match;
+	M match;
 
 	JSONValue toJSON(){
 		JSONValue ret;
@@ -38,47 +39,118 @@ public:
 	}
 }
 
-struct Match{
-	string str;
-	int priority;
-	this(string str, int priority = 0){
-		this.str = str;
-		this.priority = priority;
-	}
-}
+/// Tag a match type as being a key match (not to backtrack before it)
+/// this should usually be terminators, like semicolon, comma, bracket ends etc
+enum KeyMatch;
 
-/// matches patterns from M enum
-ASTNode!T match(M, T)(Token!T[] tokens) if (is(T == enum) && is(M == enum)){
+/// matches tokens to patterns from M enum
+public ASTNode!T match(M, T)(Token!T[] tokens) if (
+		is(T == enum) && is(M == enum)){
 	ASTNode!T ret;
-
-
+	// TODO do the thing
 	return ret;
 }
 
-private struct Unit(T, M){
-	bool isTok = false;
-	union{
-		M mat;
-		T tok;
+/// match tokens from string
+private template ASTMatches(M, T) if (is(M == enum) && is(T == enum)){
+	enum auto ASTMatches = readASTMatches();
+
+	private ASTMatch!(T, M)[] readASTMatches() pure {
+		ASTMatch!(T, M)[] ret;
+		static foreach (sym; getSymbolsByUDA!(M, string)){
+			static foreach (match; getUDAs!(sym, string)){
+				ret ~= parseASTMatch!(T, M, sym, match);
+				static if (hasUDA!(sym, KeyMatch))
+					ret[$ - 1].isKey = true;
+			}
+		}
+		return ret;
 	}
 }
 
-private struct MatchTok(T, M){
-	int priority;
-	Unit[] mat;
-	this(Unit[] match, int priority = 0){
-		this.match = match;
-		this.priority = priority;
-		this.isStr = false;
+private struct ASTMatch(T, M){
+	struct Unit{
+		bool isTok = false;
+		union{
+			M mat;
+			T tok;
+		}
+
+		this(M mat){
+			this.isTok = false;
+			this.mat = mat;
+		}
+		this(T tok){
+			this.isTok = true;
+			this.tok = tok;
+		}
+
+		string toString() const {
+			return isTok
+				? tok.to!string
+				: '/' ~ mat.to!string;
+		}
+	}
+
+	uint rootInd;
+	Unit[] matches;
+	M type;
+	bool isKey = false;
+
+	this(Unit[] matches, M type, uint rootInd = 0, bool isKey = false){
+		this.matches = matches;
+		this.type = type;
+		this.rootInd = rootInd;
+		this.isKey = isKey;
+	}
+
+	string toString() const {
+		string ret = (isKey ? "#/" : "/") ~ type.to!string ~ " - > ";
+		if (matches.length)
+			ret ~= (rootInd == 0 ? "-" : null) ~ matches[0].toString;
+		foreach (i, unit; matches[1 .. $])
+			ret ~= (rootInd == i + 1 ? " -" : " ") ~ unit.toString();
+		return ret;
 	}
 }
 
-private struct MatcherState(T){
-
-}
-
-unittest{
-	enum MatchType{
-		@Match(`Identifier`) @Match(`\Identifier Dot Identifier`) Identifier
+/// parses string into ASTMatch[]
+private ASTMatch!(T, M) parseASTMatch(T, M, M type, string str)(){
+	struct Matcher{
+		string name;
+		bool isMatched = false;
+		bool isRoot = false;
 	}
+	static Matcher parseIndividual(string str){
+		Matcher ret;
+		if (str.length && str[0] == '-'){
+			ret.isRoot = true;
+			str = str[1 .. $];
+		}
+		if (str.length && str[0] == '/'){
+			ret.isMatched = true;
+			str = str[1 .. $];
+		}
+		ret.name = str;
+		return ret;
+	}
+	alias Unit = ASTMatch!(T, M).Unit;
+
+	Unit[] units;
+	uint root;
+	static foreach (i, str; str.split!isWhite){{
+		enum auto matcher = parseIndividual(str);
+		static if (matcher.isRoot)
+			root = i;
+		static if (matcher.isMatched){
+			static assert(__traits(hasMember, M, matcher.name),
+					matcher.name ~ " not found in " ~ M.stringof);
+			units ~= Unit(__traits(getMember, M, matcher.name));
+		}else{
+			static assert(__traits(hasMember, T, matcher.name),
+					matcher.name ~ " not found in " ~ T.stringof);
+			units ~= Unit(__traits(getMember, T, matcher.name));
+		}
+	}}
+	return ASTMatch!(T, M)(units, type, root);
 }
