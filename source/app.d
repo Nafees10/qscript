cb6dc5e5ab640a250ffc9075c67239fd91a6acf9
Nafees Hassan <nafeeshassan16@gmail.com>
2023-06-30 20:12:12 +0500

    Added parsing benchmark

--- source/app.d
+++ source/app.d
@@ -2,7 +2,9 @@ module app;
 
 import std.stdio,
 			 std.file,
-			 std.algorithm.iteration;
+			 std.algorithm.iteration,
+			 std.datetime.stopwatch,
+			 std.format;
 
 import utils.ds;
 
@@ -11,7 +13,7 @@ import qscript.parser,
 			 qscript.compiler;
 
 version(unittest){}else
-void main(){
+int main(string[] args){
 	// tokenize file
 	Flags!TokenType ignore;
 	ignore |= TokenType.Whitespace;
@@ -21,6 +23,11 @@ void main(){
 
 	foreach (tok; range)
 		stderr.writeln(tok);
+
+	if (args.length > 1 && args[1] == "time"){
+		timeIt(range);
+		return 0;
+	}
 
 	Node rootNode;
 	try{
@@ -32,4 +39,40 @@ void main(){
 		writeln(rootNode.toJSON.toPrettyString);
 	else
 		stderr.writeln("root is null");
+	return 0;
+}
+
+void timeIt(Tokenizer toks){
+	void benchmark(ref StopWatch sw){
+		auto branch = toks;
+		sw.start;
+		parseScript(branch);
+		sw.stop;
+	}
+	bench(&benchmark, 100).writeln;
+}
+
+struct Times{
+	ulong min = ulong.max;
+	ulong max = 0;
+	ulong total = 0;
+	ulong avg = 0;
+	string toString() const @safe pure{
+		return format!"min\tmax\tavg\ttotal\t/msecs\n%d\t%d\t%d\t%d"(
+			min, max, avg, total);
+	}
+}
+
+Times bench(void delegate(ref StopWatch sw) func, ulong runs = 100_000){
+	Times time;
+	StopWatch sw = StopWatch(AutoStart.no);
+	foreach (i; 0 .. runs){
+		func(sw);
+		immutable ulong currentTime = sw.peek.total!"msecs" - time.total;
+		time.min = currentTime < time.min ? currentTime : time.min;
+		time.max = currentTime > time.max ? currentTime : time.max;
+		time.total = sw.peek.total!"msecs";
+	}
+	time.avg = time.total / runs;
+	return time;
 }
